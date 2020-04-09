2af6251262de6d2df5664db902b870adc9a415a9
aiitfuture1 <34682682+aiitfuture1@users.noreply.github.com>
2020-04-09 18:57:30 +0300

    Update markdown.js

--- src/markdown.js
+++ src/markdown.js
@@ -4,7 +4,6 @@ import rehypeReact from 'rehype-react'
 import Exercise from './components/exercise'
 import CodeBlock from './components/code'
 import { Link } from './components/link'
-import Slides from './components/slides'
 import Choice, { Option } from './components/choice'
 import { H3, Hr, Ol, Ul, Li, InlineCode } from './components/typography'
 
@@ -12,7 +11,6 @@ export const renderAst = new rehypeReact({
     createElement: React.createElement,
     components: {
         exercise: Exercise,
-        slides: Slides,
         codeblock: CodeBlock,
         choice: Choice,
         opt: Option,
