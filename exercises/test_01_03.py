b9829f30874bc77b15b327c3207028fb45ba1d7e
repo-code <55652829+repo-code@users.noreply.github.com>
2020-04-10 15:32:38 +0300

    Update test_01_03.py

--- exercises/test_01_03.py
+++ exercises/test_01_03.py
@@ -4,18 +4,18 @@ def test():
     
     assert(
     "x=a*b" in __solution__ or 'x = a * b' in __solution__ or "x = a*b" in __solution__ or "x= a*b" in __solution__ or "x =a*b" in __solution__
-    ), "Wrong answer: Please check x value"
+    ), "أجابة خاطئة تأكد من قيمة x"
 
     assert(
     "y=x+a" in __solution__ or "y = x + a" in __solution__ or "y = x+a" in __solution__ or "y= x+a" in __solution__ or "y =x+a" in __solution__
-    ), "Wrong answer: Please check y value"
+    ), "أجابة خاطئة تأكد من قيمة y"
 
     assert(
     "z='hello'" in __solution__ or 'z="hello"' in __solution__ or "z = 'hello'" in __solution__ or 'z = "hello"' in __solution__ or 'z= "hello"' in __solution__ or 'z ="hello"' in __solution__ or "z= 'hello'" in __solution__ or "z ='hello'" in __solution__ 
-    ), "Wrong answer: Please check z value"
+    ), "أجابة خاطئة تأكد من قيمة z"
     
     assert(
     "func(4,6,8)" in __solution__ 
     ), "Wrong answer: You chose wrong values or there are spaces between function input values. Please recheck the values or please delete the spaces e.g. func(1,2,3) "
     
-    __msg__.good("Great Answer")
+    __msg__.good("اجابة صحيحة ، احسنت")
